67209a8a62c041bccd8f993a57f02380161b4069
Yoiset2021 <yoisetabrahan@gmail.com>
2021-04-20 12:47:44 -0400

    Creando Repository

---
+++ src/components/user/repository/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Repository extends Component {
+    render() {
+        return (
+            <div>
+                Repositories
+            </div>
+        )
+    }
+}
