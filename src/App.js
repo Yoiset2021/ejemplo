1070beb3843fb94a7009666646de01d1b5d8c5da
Yoiset2021 <yoisetabrahan@gmail.com>
2021-04-20 12:52:50 -0400

    Modificando app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
+
+import Layout from './components/layout'
+import ListCommits from './components/commits'
+import Profile from './components/user/profile'
+import Repository from './components/user/repository'
 
 class App extends Component {
   
   render () {
     return (
-      <div>App</div>
+      <Layout>
+        <Route path="/commits" component={ListCommits}/>
+        <Route path="/profile" component={Profile}/>
+        <Route path="/repositories" component={Repository}/>
+      </Layout>
     );
   }
 }
