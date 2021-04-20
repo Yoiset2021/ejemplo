413a8bdd8b5f74ab701bedc15c04a12b7d8af84f
Yoiset2021 <yoisetabrahan@gmail.com>
2021-04-20 12:45:32 -0400

    Creando commits

---
+++ src/components/commits/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ListCommits extends Component {
+
+    render() {
+        return ( 
+            <div>
+                Commits
+            </div>
+        );
+    }
+}
+
+export default ListCommits
