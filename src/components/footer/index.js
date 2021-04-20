1e721629218bcf5f070595f28538e00dccd7c557
Yoiset2021 <yoisetabrahan@gmail.com>
2021-04-20 12:32:55 -0400

    Creado el footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,11 @@
+export default function footer() {
+    return (
+        <footer>
+            <div className="bg-dark text-white text-center mt-4">
+                <main className="d-flex justify-content-center align-items-center py-2">
+                    <h4> @All rigth is Reserved </h4>
+                </main> 
+            </div>
+        </footer>
+    )
+}
